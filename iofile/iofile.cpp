f3d8b78030a9328a21d9d22a72fe493a096dee67
dinaamlll <dinamaliaa17@gmail.com>
2023-06-19 14:47:45 +0700

    menunjukan ke sebuah nama file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -8,4 +8,8 @@ int main() {
 
 	//membuka file dalam mode menulis
 	ofstream outfile;
+	//menunjukan ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
