0b4957b69bd92df742cef139f3ecb4fc3066eba3
dinaamlll <dinamaliaa17@gmail.com>
2023-06-19 14:57:25 +0700

    membuka file dan menunjuk ke sebuah file

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -23,4 +23,13 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
+
 }
